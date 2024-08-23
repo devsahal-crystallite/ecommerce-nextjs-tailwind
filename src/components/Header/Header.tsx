8821beb9360da04110659ca4d1d503141510202c
devsahal-crystallite <143542001+devsahal-crystallite@users.noreply.github.com>
2024-08-23 22:00:50 +0500

    header fixs

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import {Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarShortcut, MenubarTrigger} from "@/components/ui/menubar";
 import icon1 from "media/images/header1.webp";
 import Image from "next/image";
-import {FaChevronDown, FaVirusCovid, FaMagnifyingGlass} from "react-icons/fa6";
+import {FaChevronDown, FaVirusCovid, FaMagnifyingGlass, FaHeart} from "react-icons/fa6";
 import Shipping from "media/images/shipping.png";
 import Pickup from "media/images/pickup.png";
 import Delivery from "media/images/delivery.png";
 import Spark from "media/images/spark.png";
+import Link from "next/link";
 const MenuItem = [
   {
     text: "Shipping",
@@ -25,21 +26,21 @@ const MenuItem = [
 export default function Header() {
   return (
     <header>
-      <div className="py-5 bg-[#0071dc] text-white">
+      <div className="py-2 bg-[#0071dc] text-white">
         <div className="container">
-          <div className="flex items-center justify-center gap-3 relative">
+          <div className="flex items-center justify-between gap-3 relative">
             <div className="z-10 absolute left-0 contents text-[45px] text-yellow-300 top-0">
-              <Image src={Spark} alt="icon" width={50} height={50} className="brightness-150 invert" />
+              <Image src={Spark} alt="icon" width={40} height={40} className="brightness-150 invert" />
             </div>
 
-            <Menubar className="bg-[#004F97] border-transparent rounded-full w-full justify-center items-center">
+            <Menubar className="bg-[#004F97] border-transparent rounded-full w-max justify-center items-center">
               <MenubarMenu>
                 {/* <Image src={img1} alt="icon" width={60} height={60} /> */}
-                <MenubarTrigger className="flex items-center justify-center gap-x-2 py-1">
-                  <Image src={icon1} width={35} height={35} alt="icon" />
-                  <div className="flex flex-col justify-start items-start gap-1">
-                    <span className="text-[14px] font-bold">How do you want your items?</span>
-                    <span className="text-[11px]">Sacramento, 95829 - Sacramento, Sub</span>
+                <MenubarTrigger className="flex items-center justify-between gap-x-2 py-1 h-10 w-max">
+                  <Image src={icon1} width={30} height={30} alt="icon" />
+                  <div className="flex flex-col justify-start items-start gap-0 ">
+                    <span className="text-[12px] font-bold">How do you want your items?</span>
+                    <span className="text-[10px]">Sacramento, 95829 - Sacramento, Sub</span>
                   </div>
                   <FaChevronDown />
                 </MenubarTrigger>
@@ -58,18 +59,48 @@ export default function Header() {
             </Menubar>
 
             <form action="/search" className="flex items-center justify-center px-4 w-full">
-              <div className="relative flex items-center justify-between w-full ">
+              <div className="relative flex items-center justify-between w-full h-10 ">
                 <input
                   type="search"
                   name="q"
                   placeholder="Search everything at Walmart online and in store"
-                  className="flex-1 py-4 pl-6 pr-12 border border-transparent rounded-full text-lg w-full placeholder:text-[#000]"
+                  className="flex-1 !py-2 px-6 border border-transparent rounded-full text-lg w-full placeholder:text-[#000] focus:border-none focus:shadow-none placeholder:text-[14px]"
                 />
-                <button type="reset" className="absolute right-5 p-3 cursor-pointer text-gray-500 hover:text-gray-700 w-10 h-10 bg-[#0071dc] rounded-full">
+                <button type="reset" className="absolute right-3 p-2 cursor-pointer text-gray-500 hover:text-gray-700 w-8 h-8 bg-[#0071dc] rounded-full">
                   <FaMagnifyingGlass fill="#fff" />
                 </button>
               </div>
             </form>
+
+            <div className="w-full flex items-center justify-center gap-5">
+              <Link href="/" className="hover:bg-gray-700 rounded-3xl px-5 py-1">
+                <div className="flex items-center gap-3">
+                  <FaHeart className="text-[20px]" />
+                  <div className="flex flex-col gap-y-0">
+                    <span className="p-0 m-0 text-[14px] mb-0">Reorder</span>
+                    <span className="p-0 m-0 font-bold text-[14px]">My Items</span>
+                  </div>
+                </div>
+              </Link>
+              <Link href="/" className="hover:bg-gray-700 rounded-3xl px-5 py-1">
+                <div className="flex items-center gap-3">
+                  <FaHeart className="text-[20px]" />
+                  <div className="flex flex-col gap-y-0">
+                    <span className="p-0 m-0 text-[14px]">Reorder</span>
+                    <span className="p-0 m-0 font-bold text-[14px]">My Items</span>
+                  </div>
+                </div>
+              </Link>
+              <Link href="/" className="hover:bg-gray-700 rounded-3xl px-5 py-1">
+                <div className="flex items-center gap-3">
+                  <FaHeart className="text-[20px]" />
+                  <div className="flex flex-col gap-y-0">
+                    <span className="p-0 m-0 text-[14px]">Reorder</span>
+                    <span className="p-0 m-0 font-bold text-[14px]">My Items</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
